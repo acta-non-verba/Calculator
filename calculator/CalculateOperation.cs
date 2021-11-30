f99c4431c77137721edf41e7cc1a31c83c9576a0
acta-non-verba <vikas.joshi@live.com>
2021-11-30 18:25:03 +0530

    inital commit

---
+++ calculator/CalculateOperation.cs
@@ -0,0 +1,14 @@
+namespace calculator
+{
+    public class CalculateOperation
+    {
+        public double Add(double x,double y)
+        {
+            return x+y;
+        }
+        public double Subtract(double x,double y)
+        {
+            return x-y;
+        }
+    }
+}
---
+++ calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result;
+            result= new CalculateOperation().Add(2,2);
+            result=new CalculateOperation().Subtract(3,2);
+        }
+    }
+}
